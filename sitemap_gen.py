7150b762a2eaa8c5957798b58b670a24c1358de8
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-12-07 02:56:59 -0500

    Implement mod_time function.

--- sitemap_gen.py
+++ sitemap_gen.py
@@ -3,6 +3,8 @@
 """
 import os
 from os import path
+import subprocess
+
 
 def find(root):
     """Looking for html files from a given root directory.
@@ -17,3 +19,28 @@ def find(root):
         for name in filenames:
             if name.endswith(".html"):
                 yield path.join(path.relpath(dirpath, root), name)
+
+
+def mod_time(filepath):
+    """Get the last modification of a given file path.
+
+    The returned time means seconds from the epoch time.
+
+    This method first checkes the given file are managed in the current git
+    repository, and returns its last commit time if it is found in the repository.
+    If the file isn't found in the repository, this method returns the last
+    modification time using `os.path.getmtime`.
+
+    Args:
+      filepath: absolute path to be checked the last mod time.
+
+    Returns:
+      Integer value which means seconds from the epoch time.
+    """
+    try:
+        return int(subprocess.check_output([
+            "git", "--no-pager", "log", "--pretty=%at", "-n1", filepath]))
+    except subprocess.CalledProcessError:
+        return int(path.getmtime(filepath))
+    except ValueError:
+        return int(path.getmtime(filepath))
