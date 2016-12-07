bbe7f67aeab1574ac826fc26d4606b08ba7b0881
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-12-07 02:17:45 -0500

    Add package information.

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""Package information.
+"""
+from setuptools import setup
+import sitemap_gen
+
+setup(
+    name="sitemap-gen",
+    version="0.1.0",
+    author="Junpei Kawamoto",
+    author_email="kawamoto.junpei@gmail.com",
+    description=sitemap_gen.__doc__,
+    # py_modules=[""],
+    test_suite="tests.suite",
+    license="MIT",
+    # keywords="cli helper",
+    url="https://github.com/jkawamoto/sitemap-gen",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities"
+    ]
+)
